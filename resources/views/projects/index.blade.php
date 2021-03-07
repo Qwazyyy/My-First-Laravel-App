30ddbc39fa52691f0258324f6ce226458e2c2da8
Qwazyyy <dj_kreeper@mail.ru>
2021-03-07 19:08:01 +0500

    Lesson 9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="display: flex; align-items: center;">
-        <h1 style="margin-right: auto;">AppTDD</h1>
+    <div class="flex items-center">
         <a href="/projects/create">Create New project</a>
     </div>
-	<ul>
-		@forelse($projects as $project)
-			<li>
-				<a href="{{ $project->path() }}"> {{ $project->title }} </a>
-			</li>
 
-		@empty
-			<li>No projects yet.</li>
-		@endforelse
-	</ul>
+    <div class="flex">
+        @forelse($projects as $project)
+        <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 200px">
+            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+            <div class="text-grey">{{ Illuminate\Support\Str::limit($project->description, 50) }}</div>
+        </div>
+        @empty
+            <div>No projects yet</div>
+        @endforelse
+    </div>
 @endsection
