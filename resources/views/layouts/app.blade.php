90dcf2798a9dc25ede4ccb94a381e2de698f0c06
Qwazyyy <dj_kreeper@mail.ru>
2021-03-07 20:12:37 +0500

    Lesson 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,13 +24,11 @@
         <nav class="bg-white">
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-
-{{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-{{--                    <span class="navbar-toggler-icon"></span>--}}
-{{--                </button>--}}
+                    <h1>
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="/images/log-in.png" alt="AppTDD" style="width: 100px; height: 100px;">
+                        </a>
+                    </h1>
 
                     <div>
 
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center">
-        <a href="/projects/create">Create New project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-grey text-sm font-normal">MyProject</h2>
 
-    <div class="flex">
-        @forelse($projects as $project)
-        <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 200px">
-            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-
-            <div class="text-grey">{{ Illuminate\Support\Str::limit($project->description, 50) }}</div>
+            <a href="/projects/create" class="button py-2 px-5">Create New project</a>
         </div>
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
+        @forelse($projects as $project)
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-l p-5 shadow" style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-pink pl-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
+
+                    <div class="text-grey">{{ Illuminate\Support\Str::limit($project->description, 50) }}</div>
+                </div>
+            </div>
         @empty
             <div>No projects yet</div>
         @endforelse
-    </div>
+    </main>
 @endsection
